d0774fb441834bcc003003e451eb926db6398f09
Michael <michaelp193@gmail.com>
2017-01-25 00:54:13 -0400

    add logo flip; clean css

--- prototype-grid/js/main.js
+++ prototype-grid/js/main.js
@@ -1,3 +1,6 @@
+var isTouchDevice = 'ontouchstart' in document.documentElement,
+    enterAction = isTouchDevice ? "click" : "mouseenter",
+    leaveAction = isTouchDevice ? "click" : "mouseleave";
 function addListenerToHTMLCollection (els, event, func) {
     // adds event listeners for "event" on "els" with the handler "func"
     for (i = 0; i < els.length; i += 1) {
@@ -10,8 +13,7 @@ function addListenerToHTMLCollection (els, event, func) {
 function swapEventTargetAttribute(attrA, attrB) {
     return function (event) {
 	var a = event.target.getAttribute(attrA);
-	var b = event.target.getAttribute(attrB);
-	event.target.setAttribute(attrA, b);
+	event.target.setAttribute(attrA, event.target.getAttribute(attrB));
 	event.target.setAttribute(attrB, a);
     }
 }
@@ -19,9 +21,25 @@ function swapEventTargetAttribute(attrA, attrB) {
 var swapImage = swapEventTargetAttribute("src", "data-alt-src");
 // get all the project imageelements with alt images and attach the event listeners to them
 var projectsWithAlts = Array.prototype.filter.call(
-    document.getElementsByClassName("project"),
+    document.getElementsByClassName("swap"),
     function (el) { return el.nodeName === "IMG" && el.getAttribute("data-alt-src") !== null; });
 // mouseover events from projects change the image to it's alt
-addListenerToHTMLCollection(projectsWithAlts, "mouseenter", swapImage);
+addListenerToHTMLCollection(projectsWithAlts, enterAction, swapImage);
 // mouseexit events from projects change the alt image back to the original
-addListenerToHTMLCollection(projectsWithAlts, "mouseleave", swapImage);
+addListenerToHTMLCollection(projectsWithAlts, leaveAction, swapImage);
+
+function swapVisibility (el1, el2) {
+    return function (event) {
+	el1Style = el1.style.display;
+	el1.style.display = el2.style.display;
+	el2.style.display = el1Style;
+    }
+}
+
+var text = document.getElementById("logo-text");
+var logo = document.getElementById("logo");
+
+var swapAbout = swapVisibility(text, logo);
+var aboutSection = document.getElementById("about")
+aboutSection.addEventListener(enterAction, swapAbout);
+aboutSection.addEventListener(leaveAction, swapAbout);
