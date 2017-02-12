339a7a77b146ba9a28158bafc35e0646476a4134
Michael <michaelp193@gmail.com>
2017-02-12 14:10:46 -0400

    clean code; make more responsive

--- prototype-grid/js/main.js
+++ prototype-grid/js/main.js
@@ -1,6 +1,7 @@
 var isTouchDevice = 'ontouchstart' in document.documentElement,
     enterAction = isTouchDevice ? "click" : "mouseenter",
-    leaveAction = isTouchDevice ? "click" : "mouseleave";
+    exitAction = isTouchDevice ? "click" : "mouseleave";
+
 function addListenerToHTMLCollection (els, event, func) {
     // adds event listeners for "event" on "els" with the handler "func"
     for (i = 0; i < els.length; i += 1) {
@@ -8,38 +9,30 @@ function addListenerToHTMLCollection (els, event, func) {
     }
 }
 
-
-
 function swapEventTargetAttribute(attrA, attrB) {
     return function (event) {
+        console.log("click");
 	var a = event.target.getAttribute(attrA);
 	event.target.setAttribute(attrA, event.target.getAttribute(attrB));
 	event.target.setAttribute(attrB, a);
     }
 }
 
+function isImageWithAlt (el) {
+    return el.nodeName === "IMG" && el.getAttribute("data-alt-src") !== null;
+}
+
 var swapImage = swapEventTargetAttribute("src", "data-alt-src");
+
 // get all the project imageelements with alt images and attach the event listeners to them
 var projectsWithAlts = Array.prototype.filter.call(
     document.getElementsByClassName("swap"),
-    function (el) { return el.nodeName === "IMG" && el.getAttribute("data-alt-src") !== null; });
+    isImageWithAlt);
+
 // mouseover events from projects change the image to it's alt
 addListenerToHTMLCollection(projectsWithAlts, enterAction, swapImage);
-// mouseexit events from projects change the alt image back to the original
-addListenerToHTMLCollection(projectsWithAlts, leaveAction, swapImage);
 
-function swapVisibility (el1, el2) {
-    return function (event) {
-	el1Style = el1.style.display;
-	el1.style.display = el2.style.display;
-	el2.style.display = el1Style;
-    }
+if (enterAction !== exitAction) {
+    // mouseexit events from projects change the alt image back to the original
+    addListenerToHTMLCollection(projectsWithAlts, exitAction, swapImage);
 }
-
-var text = document.getElementById("logo-text");
-var logo = document.getElementById("logo");
-
-var swapAbout = swapVisibility(text, logo);
-var aboutSection = document.getElementById("about")
-aboutSection.addEventListener(enterAction, swapAbout);
-aboutSection.addEventListener(leaveAction, swapAbout);
